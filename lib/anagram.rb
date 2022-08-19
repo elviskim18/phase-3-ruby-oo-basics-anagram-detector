9f2cf2731439fd7428e6ba7a49d4c7fdbaa5d9e0
Kimani <elvis.kimani@student.moringaschool.com>
2022-08-19 21:19:41 +0300

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
     end
 
     def match(array)
-        array.select do |letter|
+        array.filter do |letter|
             letter.split("").sort == word.split("").sort
          end
     end
